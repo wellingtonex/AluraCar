afadc2f6c98ea43e0f58aa8929b6378029044b36
wellington.siqueira <wellingtonex@gmail.com>
2016-12-15 15:20:22 -0300

    criando um controller para a tela de listagem de carros

---
+++ readme.txt
@@ -0,0 +1,7 @@
+podemos tambem utilizar 
+
+ionic start nome_do_projeto blank
+ionic start nome_do_projeto tabs
+ionic start nome_do_projeto sidemenu
+
+ng-show, ng-hide e a ng-if
---
+++ www/js/listagem-controler.js
@@ -0,0 +1,17 @@
+angular.module('starter')
+    .controller('ListagemController', $scope => {
+
+        $scope.carros = [
+          'BMW 120i',
+          'Onix 1.6',
+          'Fiest 2.0',
+          'C3 1.0',
+          'Uno Fire',
+          'Sentra 2.0',
+          'Astra Sedan',
+          'Vectra 2.0 turbo',
+          'Hilux 4x4',
+          'Montana Cabine dupla',
+        ];
+
+    });
