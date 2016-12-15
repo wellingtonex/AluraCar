28753420a8a49209f9795ca87304e57e0862f3f5
wellington.siqueira <wellingtonex@gmail.com>
2016-12-15 18:53:42 -0300

    criando view de detalhe dos pedidos

---
+++ www/js/detalhe-controller.js
@@ -0,0 +1,4 @@
+angular.module('starter')
+.controller('DetalheController', function($scope){
+
+});
--- www/js/routes.js
+++ www/js/routes.js
@@ -4,11 +4,15 @@ angular.module('starter')
   $urlRouterProvider.otherwise('/listagem');
 
   $stateProvider
-
   .state('listagem', {
     url: '/listagem',
     templateUrl: 'templates/listagem.html',
     controller: 'ListagemController'
+  })
+  .state('detalhe', {
+    url: '/detalhe',
+    templateUrl: 'templates/detalhe.html',
+    controller: 'DetalheController'
   });
   
 });
