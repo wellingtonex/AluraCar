de5175bd47f112834bf4c70e1e2fbf3177fabff8
wellington.siqueira <wellingtonex@gmail.com>
2016-12-15 18:21:49 -0300

    trabalhando com rotas e com <ion-nav-view></ion-nav-view>

--- www/js/listagem-controler.js
+++ www/js/listagem-controler.js
@@ -1,17 +1,8 @@
 angular.module('starter')
-    .controller('ListagemController', $scope => {
+.controller('ListagemController', function($scope){
 
-        $scope.carros = [
-          'BMW 120i',
-          'Onix 1.6',
-          'Fiest 2.0',
-          'C3 1.0',
-          'Uno Fire',
-          'Sentra 2.0',
-          'Astra Sedan',
-          'Vectra 2.0 turbo',
-          'Hilux 4x4',
-          'Montana Cabine dupla',
-        ];
+	$scope.carros =  ["BMW 120i", "Onix 1.6", "Fiesta 2.0", "C3 1.0", "Uno Fire", "Sentra 2.0",
+								"Astra Sedan", "Vectra 2.0 Turbo", "Hilux 4x4", "Montana Cabine dupla",
+								"Outlander 2.4", "Fusca 1500"];
 
-    });
+});
---
+++ www/js/routes.js
@@ -0,0 +1,14 @@
+angular.module('starter')
+.config(function($stateProvider, $urlRouterProvider) {
+  
+  $urlRouterProvider.otherwise('/listagem');
+
+  $stateProvider
+
+  .state('listagem', {
+    url: '/listagem',
+    templateUrl: 'templates/listagem.html',
+    controller: 'ListagemController'
+  });
+  
+});
