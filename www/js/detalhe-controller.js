eae3343cb64d43b4d63dc91cbd197fc955a79f59
wellington.siqueira <wellingtonex@gmail.com>
2016-12-15 22:26:39 -0300

    finalizando implementação do calculo do valor do veiculo selecionado juntamento com o valor dos itens selecionados.

--- www/js/detalhe-controller.js
+++ www/js/detalhe-controller.js
@@ -2,4 +2,19 @@ angular.module('starter')
 .controller('DetalheController', function($scope, $stateParams){
 
     $scope.carro = angular.fromJson($stateParams.carro);
+    $scope.precoFinal = 0;
+
+    $scope.acessorios = [
+        {nome:'Freio ABS', preco:800},
+        {nome:'Ar Condicionado', preco:1000},
+        {nome:'MP3 Player', preco:500}
+    ];
+
+    $scope.changeItem = function(acessorio, isMarcado) {
+        if(isMarcado) {
+            $scope.carro.preco += acessorio.preco;
+        } else {
+            $scope.carro.preco -= acessorio.preco;
+        }
+    }
 });
--- www/js/listagem-controler.js
+++ www/js/listagem-controler.js
@@ -2,17 +2,16 @@ angular.module('starter')
 .controller('ListagemController', function($scope){
 
 	$scope.carros =  [
-		{nome:"BMW 120i",preco:50000},
-		{nome:"Onix 1.6",preco:29000},
-		{nome:"Fiesta 2.0",preco:42000},
-		{nome:"C3 1.0",preco:47000},
-		{nome:"Uno Fire",preco:25000},
-		{nome:"Sentra 2.0",preco:36000},
-		{nome:"Astra Sedan",preco:18000},
-		{nome:"Vectra 2.0 Turbo",preco:28000},
-		{nome:"Hilux 4x4",preco:59000},
-		{nome:"Montana Cabine dupla",preco:34000},
-		{nome:"Outlander 2.4",preco:60000},
-		{nome:"Fusca 1500",preco:12000}];
-
+		{"nome" : "BMW 120i" , "preco" : 70000},
+		{"nome" : "Onix 1.6" , "preco" : 35000},
+		{"nome" : "Fiesta 2.0", "preco" : 52000},
+		{"nome" : "C3 1.0", "preco" : 22000},
+		{"nome" : "Uno Fire", "preco" : 11000},
+		{"nome" : "Sentra 2.0", "preco" : 53000},
+		{"nome" : "Astra Sedan", "preco" : 39000},
+		{"nome" : "Vectra 2.0 Turbo", "preco" : 37000},
+		{"nome" : "Hilux 4x4", "preco" : 90000},
+		{"nome" : "Montana Cabine dupla", "preco" : 57000},
+		{"nome" : "Outlander 2.4" ,"preco" : 99000},
+		{"nome" : "Fusca 1500", "preco" : 6000}];
 });
