37dfd7919a4b13e522010cfcced872209f84132e
wellington.siqueira <wellingtonex@gmail.com>
2016-12-16 19:07:20 -0300

    criando popup com mensagem de pedido finalizado com sucesso.

--- www/js/finaliza-pedido-controller.js
+++ www/js/finaliza-pedido-controller.js
@@ -1,7 +1,15 @@
 angular
     .module('starter')
-    .controller('FinalizaPedidoController', function ($scope, $stateParams) {
+    .controller('FinalizaPedidoController', function ($scope, $stateParams, $ionicPopup, $state) {
          $scope.carro = angular.fromJson($stateParams.carro);
          console.log($scope.carro);
          
+         $scope.finalizarPedido = function() {
+            $ionicPopup.alert({
+                title: 'Parabéns',
+                template: 'Você acaba de comprar um carro.'
+            }).then(function() {
+                $state.go('listagem');
+            })
+         }
     });
